a31cfd90e01897334dbe9cca67605f79de0fc19b
anastasia <anastasiawalia@gmail.com>
2016-05-11 11:15:49 -0700

    Routing and States

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,30 @@
-angular.module('blocJams', []);
+ (function() {
+     function config($stateProvider, $locationProvider) {
+         $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+
+     $stateProvider
+         .state('landing', {
+             url: '/landing',
+             templateUrl: '/app/templates/landing.html'
+                  })
+     
+         .state('collection', {
+             url: '/collection',
+             templateUrl: '/app/templates/collection.html'
+                  })
+     
+         .state('album', {
+             url: '/album',
+             templateUrl: '/app/templates/album.html'
+         
+         });
+ }
+ 
+     angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+ })();
